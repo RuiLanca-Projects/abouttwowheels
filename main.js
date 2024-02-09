5670ec133ee3b1598b9d7a55239fb574df22036b
RuiLanca-Projects <129678863+RuiLanca-Projects@users.noreply.github.com>
2024-02-09 20:06:47 +0000

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function() {
   }, 2000); 
 });
 
-if (window.location.href.endsWith('index.php')) {  
+if (window.location.href.endsWith('index.html')) {  
   window.onload = function() {
     setTimeout(function() {
       window.location.href = 'inicio.html';
