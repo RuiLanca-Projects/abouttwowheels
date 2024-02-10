01111b7dbbf8036707f3db8245721ca3d62b0dae
RuiLanca-Projects <129678863+RuiLanca-Projects@users.noreply.github.com>
2024-02-10 20:54:04 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function() {
   }, 2000); 
 });
 
-if (window.location.href.endsWith('abouttwowheels/' || 'index.html')) {  
+if (window.location.href.endsWith('abouttwowheels/') || window.location.href.endsWith('index.html'))) {  
   console.log("Olá")
   window.onload = function() {
     setTimeout(function() {
